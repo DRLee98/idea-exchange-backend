0ec5f005cd3d396d078f999b2884a62fa3ee7b5a
Lee Dongryeol <dongyeol01@naver.com>
2021-10-26 17:07:40 +0900

    서버 코드 수정
    업로드 추가로 서버 코드 수정

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,22 @@
-import { ApolloServer } from "apollo-server";
+import "dotenv/config";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { graphqlUploadExpress } from "graphql-upload";
 import { schema } from "./schema";
+
+const PORT = process.env.PORT;
 
 const server = new ApolloServer({ schema });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+server.start().then(() => {
+  const app = express();
+
+  app.use("/static", express.static("files"));
+  app.use(graphqlUploadExpress());
+
+  server.applyMiddleware({ app });
+
+  app.listen({ port: PORT });
+
+  console.log(`🚀 Server is running on http://localhost:${PORT}/graphql`);
 });
